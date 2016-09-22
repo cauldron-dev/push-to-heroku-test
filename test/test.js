da908f238389d9dbb626720b371f795229e51cb4
cauldron-dev <cauldron-dev@menloinnovations.com>
2016-09-22 10:54:48 -0400

    added tests

---
+++ test/test.js
@@ -0,0 +1,9 @@
+"use strict";
+var assert = require('assert');
+
+describe('nonsense tests', function(){
+    it('should fail', function(done){
+        assert(false);    
+        done();
+    });
+});
